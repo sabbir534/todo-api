4a87ee1da0bd07d43a16c3987767aa1834e8b5ea
Sabbir Ahmed <prince534534@gmail.com>
2015-12-05 05:32:40 +0600

    Add UPDATE functionality

--- server.js
+++ server.js
@@ -60,6 +60,34 @@ app.delete('/todos/:id', function(req, res){
 	}
 });
 
+// Update /todos/:id
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributs = {};
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributs.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributs.description = body.description.trim();
+	} else if(body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributs);
+	res.json(matchedTodo);
+
+
+});
+
 app.listen(PORT, function(){
 	console.log('Listening Port ' + PORT);
 })
